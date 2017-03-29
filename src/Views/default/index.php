953f73b8a273ac31d408be058e7f6c10830b975c
Petr Karmashev <smonkl@bk.ru>
2017-03-29 16:51:52 +0300

    Index page styling

--- src/Views/default/index.php
+++ src/Views/default/index.php
@@ -1,1 +1,14 @@
-<p>index page</p>
+<header>Lenvendo Paint</header>
+
+<div class="image-box">
+    <div class="image"></div>
+    <div class="image"></div>
+    <div class="image"></div>
+    <div class="image"></div>
+    <div class="image"></div>
+    <div class="image"></div>
+    <div class="image"></div>
+    <div class="image"></div>
+    <div class="image"></div>
+    <div class="image"></div>
+</div>
--- src/Views/layouts/main.php
+++ src/Views/layouts/main.php
@@ -8,6 +8,8 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Lenvendo Paint</title>
+    <link rel="stylesheet" href="styles.css">
+    <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed" rel="stylesheet">
 </head>
 <body>
 
